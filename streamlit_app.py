a56f511ab5d6310754887344db43204bec127d2d
harshSethi11 <154815252+harshSethi11@users.noreply.github.com>
2025-11-27 21:51:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ API_URL = "http://localhost:8000/analyze"
 
 st.set_page_config(page_title="Support Ticket Analyzer", layout="wide")
 
-st.title("📩 Support Ticket Analyzer")
+st.title("Support Ticket Analyzer")
 st.write("This app classifies customer support issues and generates a summary using Transformer models.")
 
 # Input box
@@ -15,22 +15,23 @@ text = st.text_area("Paste customer support conversation or ticket:", height=280
 # Button
 if st.button("Analyze"):
     if not text.strip():
-        st.error("⚠️ Please enter some ticket text.")
+        st.error("Please enter some ticket text.")
     else:
-        with st.spinner("Analyzing... ⏳"):
+        with st.spinner("Analyzing.."):
             response = requests.post(API_URL, json={"text": text})
 
             if response.status_code != 200:
-                st.error("❌ Error calling API")
+                st.error("Error calling API")
             else:
                 data = response.json()
 
                 col1, col2 = st.columns(2)
 
                 with col1:
-                    st.subheader("📊 Sentiment")
+                    st.subheader("Sentiment")
                     st.write(f"**Sentiment:** {data['sentiment']}")
 
                 with col2:
-                    st.subheader("📝 Summary")
+                    st.subheader("Summary")
                     st.success(data['summary'])
+
